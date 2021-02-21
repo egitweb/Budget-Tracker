1fa25896be99f25e05fbcb89e5e41b8619568678
Eugenio Espinoza <68721930+egitweb@users.noreply.github.com>
2021-02-21 12:39:44 -0600

    service-worker.js update

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,3 +13,15 @@ const FILES_TO_CACHE = [
 ];
 
 
+self.addEventListener("install", function (evt) {    
+    // pre cache all static assets
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+  
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
+    self.skipWaiting();
+  });
+  
+
