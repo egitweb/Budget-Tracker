107daf7f1fd02fdf03096273558e53eb9f0ad52a
Eugenio Espinoza <68721930+egitweb@users.noreply.github.com>
2021-02-21 14:37:52 -0600

    service-work update

--- public/service-worker.js
+++ public/service-worker.js
@@ -25,3 +25,28 @@ self.addEventListener("install", function (evt) {
   });
   
 
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+
+  self.addEventListener('fetch', function(evt) {
+    // code to handle requests goes here
+
+
+    
+    });
+    
